f06173a8066380b29d76b5fc345bc369df9f94c2
moonsbeen626 <moonsbeen@naver.com>
2021-07-01 22:17:10 +0900

    Initial commit

---
+++ src/main/java/moon/clone/instargram/InstargramApplication.java
@@ -0,0 +1,13 @@
+package moon.clone.instargram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstargramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstargramApplication.class, args);
+    }
+
+}
---
+++ src/test/java/moon/clone/instargram/InstargramApplicationTests.java
@@ -0,0 +1,13 @@
+package moon.clone.instargram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstargramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
